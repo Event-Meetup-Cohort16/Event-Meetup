875c68832a1f191258269466aa88cb370159d27c
Jen Saxena <jen@jensaxena.com>
2017-11-28 18:37:59 -0500

    Create empty components for Event Tiles

---
+++ dev/scripts/event-tile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class EventTile extends React.Component {
+    render() {
+      return (
+        <div>
+          {/* Event Tile goes here */}
+        </div>
+      )
+    }
+}
