b675fbcb3e6afc26fb0a23e2633c693744fd46a2
Kara Barleta <karabarleta@gmail.com>
2017-11-28 17:48:27 -0500

    footer component added

---
+++ dev/scripts/footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    NavLink
+} from 'react-router-dom';
+
+class Footer extends React.Component {
+
+    constructor() {
+        super();
+        this.goBack = this.goBack.bind(this)
+    }
+
+    goBack(e) {
+        e.preventDefault();
+        // This loads the most previous page based on browser history
+        window.history.back();
+    }
+    
+    render(){
+        return(
+            <ul>
+                <li>
+                    <NavLink to="/home">My Events </NavLink>
+                    <NavLink to="/search">Search</NavLink>
+                    <a href="#" onClick={this.goBack}>Back</a>
+                </li>
+            </ul>
+        )
+    }
+}
+
+export default Footer;
