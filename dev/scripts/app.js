8af24a7cdd6b475e2087e66099fd1ec8a41ddbbf
Simon Steer <simonpsteer@gmail.com>
2017-11-28 18:41:45 -0500

    set up login authentication, initialized firebase, set up routing on main app component

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Login from './login.js';
+import UserEvents from './userevents.js';
+import SearchEvents from './searchevents.js';
+import EventTile from './eventtile.js';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+var config = {
+  apiKey: "AIzaSyDd8y8IyT-l37rZ1mZjqvrVLTUIICKMMlY",
+  authDomain: "what-s-the-haps.firebaseapp.com",
+  databaseURL: "https://what-s-the-haps.firebaseio.com",
+  projectId: "what-s-the-haps",
+  storageBucket: "what-s-the-haps.appspot.com",
+  messagingSenderId: "265551062294"
+};
+firebase.initializeApp(config);
+
 class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      // state.user will be used to keep track of who is logged in (or if nobody is logged in)
+      user: '',
+      // state.currentpage will be used to keep track of where the user is on the website, and will also be used to conditionally render parts of the EventTile component.
+      currentpage: ''
+    }
+    this.currentUser = this.currentUser.bind(this);
+  }
+  
+  // This method will get passed down as a prop on the Login component, and will be used to update the user state on the main App component depending on if a user is logged in or not.
+  currentUser (uid) {
+    this.setState({
+      user: uid
+    })
+  }
+
     render() {
       return (
-        <div>
-          Please work????
-        </div>
+        <Router>
+          <div>
+            <Route exact path="/" component={Login} />
+            <Route path="/home" component={UserEvents} />
+            <Route path="/search" component={SearchEvents} />
+            <Route path="/event" component={EventTile} />
+          </div>
+        </Router>
       )
     }
 }
---
+++ dev/scripts/login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const provider = new firebase.auth.GoogleAuthProvider()
+
+// Login component will get rendered on the main app component
+export default class Login extends React.Component {
+    constructor() {
+        super();
+        this.login = this.login.bind(this)
+    }
+
+    // Login Method
+    login(e) {
+        e.preventDefault();
+        firebase.auth().signInWithPopup(provider)
+            .then((user) => {
+                // When the user logs in, set the user state on app.js to the user id
+                this.props.setUser({
+                    ID: user.uid,
+                    email: user.email
+                })
+            })
+    }
+
+    // Logout Method
+    logout(e) {
+        e.preventDefault();
+        firebase.auth().signOut()
+            .then(() => {
+                // When the user logs out, set the user state on app.js to an empty string
+                this.props.setUser('')
+            })
+    }
+
+    // Check if the user is logged in or not when they visit the page and when the component mounts
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                // Set the user state on app.js to an object with the user ID and email
+                this.props.setUser({
+                    ID: user.uid,
+                    email: user.email
+                })
+            } else {
+                // If not logged in, set the user state on app.js to an empty string
+                this.props.setUser('')
+            }
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <a href="" onClick={this.login}>Login</a>
+                <br />
+                <a href="" onClick={this.logout}>Logout</a>
+            </div>
+        )
+    }
+}
