1dabd0ee2ceec9ed116fbd6b129c30ab33c1bc4c
Jen Saxena <jen@jensaxena.com>
2017-12-01 14:44:27 -0500

    Firing data at Firebase

--- dev/scripts/invite-user.js
+++ dev/scripts/invite-user.js
@@ -5,21 +5,19 @@ export default class InviteUser extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
-        user: '',
         // friend's email goes here:
-        userFriend: '',
+        friend: '',
       }
-      // we need to set initial state of user to the current user's email
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
     handleChange(e) {
-      this.setState({ userFriend: e.target.value });
+      this.setState({ friend: e.target.value });
     };
     handleSubmit(e) {
       e.preventDefault();
-      this.props.submitEmail(this.state.userFriend);
-      this.setState({ userFriend: '' });
+      this.props.submitEmail(this.state.friend);
+      this.setState({ friend: '' });
     };
     render() {
       return (
@@ -33,7 +31,7 @@ export default class InviteUser extends React.Component {
               onChange={this.handleChange}
               placeholder="Enter a friend's email to send them the haps!"
               type="email"
-              value={this.state.userFriend} />
+              value={this.state.friend} />
             <button type="submit">Send Invite</button>
           </form>
         </div>
