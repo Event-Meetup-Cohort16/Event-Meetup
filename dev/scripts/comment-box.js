fba525cc37ea4db8fbf720658dae24c17ca03d86
Kara Barleta <karabarleta@gmail.com>
2017-12-04 10:57:29 -0500

    update comment box path

--- dev/scripts/comment-box.js
+++ dev/scripts/comment-box.js
@@ -9,41 +9,66 @@ export default class CommentBox extends React.Component {
     super();
     this.state = {
       comments: [],
+      host:'',
+      eventID: ''
+
     }
   this.handleCommentSubmit = this.handleCommentSubmit.bind(this)
   }
 
-  addComments(commentInfo) { // adding comments to database
+ // Displaying comments already stored in database
+  componentDidMount() { 
+    // We start by searching through the users database for the current event and retrieving the host information
+    var user = this.props.userEmail.replace(/\./g, ',')
+    var eventID = `1A8ZAugGkdcikMK`// ****TO UPDATE WITH SPECIFIC EVENT ID
+    // var eventID = this.props.eventID// ****TO UPDATE WITH SPECIFIC EVENT ID
+    //1AvZZfsGkDdm6ES --> hardcoded cars
+    //1AvZZfsGkDdm6ES --> hardcoded cars
+    console.log(eventID) // ***TO UPDATE WITH SPECIFIC EVENTID
+    const rootRef = firebase.database().ref(`users/${user}/events/${eventID}/host`)
+    rootRef.on('value', (snapshot) => {
+        console.log(snapshot.val())
+        var host = snapshot.val()
+      
+      
+      // Once we have the host information, we store all the comments regarding the specific event page in that HOST's node (this is because the host is the original person who created the event page)
+      const userRef = firebase.database().ref(`users/${host}/events/${eventID}/comments`);
+        userRef.on('value', (snapshot) => {
+          const firebaseData = snapshot.val()
+          const commentData = []
+          for (let commentKey in firebaseData) {
+            commentData.push(firebaseData[commentKey])
+          }
+          
+          this.setState({
+            comments: commentData
+          })
+          this.setState({
+            host : host
+          })
+          this.setState({
+            eventID : eventID
+          })
+
+        }) // closing userRef
+    }) //closing rootRef
+  }
+
+  // Adding comments to database
+  addComments(commentInfo) { 
     console.log(commentInfo)
+    // Host information and eventID is stored in state for every comment created
+    const dbRef = firebase.database().ref(`users/${commentInfo.host}/events/${commentInfo.event}/comments`);
 
-    const dbRef = firebase.database().ref(`users/host/events/eventID/comments`);
-
-    dbRef.push(commentInfo).then((snapshot) => {
-      let newKey = snapshot.key;
-      let updateShoeRef = firebase.database().ref(`users/host/events/eventID/comments/${newKey}`);
-      commentInfo.key = newKey;
-      //updates comment with property of "key" (i.e. key: key)
-      updateShoeRef.update(commentInfo);
-    })
+    dbRef.push(commentInfo)
   }
-  componentDidMount() { // showing comments already stored in database
-    const userRef = firebase.database().ref(`users/host/events/eventID/comments`);
-    userRef.on('value', (snapshot) => {
-      // console.log(snapshot.val())
-      const firebaseData = snapshot.val()
-      const commentData = []
-      for (let commentKey in firebaseData) {
-        commentData.push(firebaseData[commentKey])
-      }
-      this.setState({
-        comments: commentData
-      })
-    })
 
-  }
+
 
   handleCommentSubmit(comment) {
       console.log(this.props.userEmail)
+      console.log(this.props.eventID)
+      // On submit of the Comment-Form, we create a timestamp.
       const timeStamp = () => {
         let options = {
           month: '2-digit',
@@ -55,21 +80,12 @@ export default class CommentBox extends React.Component {
         let now = new Date().toLocaleString('en-US', options);
         return now;
       }
-      // get user email from app component(to be passed down as props)
-      // const userLoggedIn = this.props.userEmail
-      // console.log(userLoggedIn)
-
       var DateTime = timeStamp()
-      console.log(DateTime)
-
-
-
       var comments = this.state.comments;
       comment.id = Date.now();
       comment.time = DateTime;
-      // comment.author = this.props.userEmail; // Sets the commentbox author to whoever is logged in
 
-
+      // When new comments are submitted, update state to included previous comments creating an array of comments objects
       var newComments = comments.concat([comment]);
       this.setState({comments: newComments});
       console.log(this.state)
@@ -83,10 +99,14 @@ export default class CommentBox extends React.Component {
       <div className="commentBox__div" >
         <h2 className="commentBox__head--comments">Comments</h2>
 
-        <CommentForm onCommentSubmit={this.handleCommentSubmit} submitForm={this.addComments} user={this.props.userEmail}
-          // userLoggedIn={userLoggedIn}
+        <CommentForm 
+        onCommentSubmit={this.handleCommentSubmit} 
+        submitForm={this.addComments} 
+        user={this.props.userEmail}
+        // Host & EventID stored as state, passed down as props to Comment-Form to be used in the addComments method
+        host={this.state.host}
+        eventID={this.state.eventID}
         />
-
         <CommentList comments={this.state.comments} />
 
       </div>
