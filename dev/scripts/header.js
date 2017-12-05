5ac0236f9925c0ef4f7d4d18bd56e1e7d5810bef
Kara Barleta <karabarleta@gmail.com>
2017-12-05 10:09:51 -0500

    some styling added

--- dev/scripts/header.js
+++ dev/scripts/header.js
@@ -12,7 +12,9 @@ export default class Header extends React.Component {
     return (
       <div className="header__div clearfix">
         <img className="header__img--logo" src="public/images/noun_1061378_cc_color_150x85.png" alt="The Haps logo"/>
-        <h1 className="header__head--brand">The Haps <span className="header__span--tagline">Event meetups with friends!</span></h1>
+        <h1 className="header__head--brand">The Haps 
+        <span className="header__span--tagline">Event meetups with friends!</span>
+        </h1>
       </div>
     )
   }
